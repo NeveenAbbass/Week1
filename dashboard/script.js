4a1ab996a9b8462444638eb07b31435da510e64c
Neveen Abbas <neveen.abbas555@gmail.com>
2024-12-31 12:58:13 +0200

    fixed the issues apparent in task due to failed pagination

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,22 +1,6 @@
-// * Simple User Management Dashboard
-// 	. Fetch data from https://jsonplaceholder.typicode.com/users using the fetch API
-// 	. Display a loading message while fetching the data
-// 	. Handle errors gracefully if the fetch request fails
-// 	. Create table with the following columns (Name, Email, Phone, Company Name) Styling is a plus
-// 	.Use forEach to iterate over the user data and populate the rows of the table dynamically
-// 	. Use map to extract the names of all companies into an array
-// 	. Display these company names as a comma-separated list below the table
-
-// 	^ Bonus Features:
-// 		-Search Functionality: Add an input field to search users by name. Highlight matching rows in the table.
-// ?		-Pagination: Display 5 users per page with "Next" and "Previous" buttons.
-
 // varibales setting
 let loader = document.getElementById("loader");
 let table = document.getElementById("usersTable");
-let nextBtn = document.getElementById("next") 
-let prevBtn = document.getElementById("prev") 
-let page = 1;
 let data = [];
 
 
@@ -27,7 +11,7 @@ async function fetchData() {
 }
 
 fetchData().then((data) => {
-  tableConstruct(data,page);
+  tableConstruct(data);
   mappingCompanies(data);
   searchByName();
 })
@@ -37,36 +21,17 @@ fetchData().then((data) => {
 
 })
 
-nextBtn.addEventListener("click", () => {
-  const totalPages = Math.ceil(data.length / 5); 
-  if (page < totalPages) {
-      page++;
-      tableConstruct(data, page);
-  }
-});
 
-  prevBtn.addEventListener("click",(e)=>{
-    if(page > 1)
-      page--;
-    tableConstruct(data,page);
-  console.log(page);
-
-  })
-
-function tableConstruct(data,page) {
+function tableConstruct(data) {
   //------------hiding the loader and displaying the UI elements
   loader.style.display = "none";
   table.style.display = "table";
   document.getElementById("search").style.display = "block";
-  document.querySelector(".btn-container").style.display = "flex";
 
   const tableBody = document.getElementById("tableBody");
-  tableBody.innerHTML = "";
 
-  let paginationParams = pagination(data,5,page);
-  
-  
-  paginationParams[0].forEach((element) => {
+  // ?tableBody.innerHTML = "";
+  data.forEach((element) => {
     //---------putting data into dynamic table
     document.getElementById("tableBody").innerHTML += `
 <tr>
@@ -85,19 +50,6 @@ function tableConstruct(data,page) {
 function mappingCompanies(data){
   let companies = data.map((user) => user.company.name).join(",");
   document.querySelector(".container").innerHTML += `<div class="comp-list"><span> compnaies list: </span><br><br>  ${companies} </div>`;
-}
-
-
-//-----------------pagination------------------
-function pagination(data,rows,page){
-  var trimStart = (page -1) * rows;
-  var trimEnd = trimStart + rows;
-
-  var trimmedData = data.slice(trimStart, trimEnd);
-
-  var pages = Math.ceil(data.length / rows);
-
-  return [trimmedData, pages];
 }
 
 
